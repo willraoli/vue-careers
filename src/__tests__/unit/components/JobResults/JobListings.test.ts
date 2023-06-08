f811d0016aadc42c6c3d04e5213065df3b98388b
William <48337511+willraoli@users.noreply.github.com>
2023-06-08 20:25:16 -0300

    Add more tests to JobListings

--- src/__tests__/unit/components/JobResults/JobListings.test.ts
+++ src/__tests__/unit/components/JobResults/JobListings.test.ts
@@ -9,7 +9,7 @@ vi.mock("axios");
 describe("JobListings", () => {
   const createRoute = (queryParams = {}) => ({
     query: {
-      page: "5"
+      page: "1"
     },
     ...queryParams
   });
@@ -40,5 +40,59 @@ describe("JobListings", () => {
 
     const jobListings = await screen.findAllByRole("listitem");
     expect(jobListings).toHaveLength(10);
+  });
+
+  describe("quando não tem page nos params", () => {
+    it("mostra a página 1", () => {
+      const queryParams = { query: { page: undefined } };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      expect(screen.getByText("Página 1")).toBeInTheDocument();
+    });
+  });
+
+  describe("quando tem page nos params", () => {
+    it("mostra o número da página", () => {
+      const queryParams = { query: { page: "3" } };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      expect(screen.getByText("Página 3")).toBeInTheDocument();
+    });
+  });
+
+  describe("quando o usuário está na primeira página", () => {
+    it("não mostra o botão Anterior", async () => {
+      (axios.get as Mock).mockResolvedValue({ data: Array(15).fill({}) });
+      const queryParams = { query: { page: "1" } };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      await screen.findAllByRole("listitem");
+      const previousText = screen.queryByRole("link", {
+        name: /anterior/i
+      });
+
+      expect(previousText).not.toBeInTheDocument();
+    });
+
+    it("mostra o botão Próxima", async () => {
+      (axios.get as Mock).mockResolvedValue({ data: Array(15).fill({}) });
+      const queryParams = { query: { page: "1" } };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      await screen.findAllByRole("listitem");
+      const nextText = screen.queryByRole("link", {
+        name: /próxima/i
+      });
+
+      expect(nextText).toBeInTheDocument();
+    });
   });
 });
