a808f221f2b839b13f79a55b84b0bd72d142f172
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-08 17:11:34 -0300

    Refactor JobListings.test with queryParams

--- src/__tests__/unit/components/JobResults/JobListings.test.ts
+++ src/__tests__/unit/components/JobResults/JobListings.test.ts
@@ -7,21 +7,36 @@ import { RouterLinkStub } from "@vue/test-utils";
 vi.mock("axios");
 
 describe("JobListings", () => {
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: "5"
+    },
+    ...queryParams
+  });
+
+  const renderJobListings = ($route: {}) => {
+    return render(JobListings, {
+      global: {
+        stubs: {
+          "router-link": RouterLinkStub
+        },
+        mocks: {
+          $route
+        }
+      }
+    });
+  };
+
   it("dá fetch nas vagas", () => {
     (axios.get as Mock).mockResolvedValue({ data: [] });
-    render(JobListings);
+    renderJobListings(createRoute());
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("cria no máximo 10 anúncios", async () => {
     (axios.get as Mock).mockResolvedValue({ data: Array(15).fill({}) });
-    render(JobListings, {
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub
-        }
-      }
-    });
+    const queryParams = { query: { page: "1" } };
+    renderJobListings(createRoute(queryParams));
 
     const jobListings = await screen.findAllByRole("listitem");
     expect(jobListings).toHaveLength(10);
