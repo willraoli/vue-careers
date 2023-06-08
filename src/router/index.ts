79afa4260a001caa1383589b14ea8ed5026aba17
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-08 17:11:46 -0300

    Add scroll behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior() {
+    return { top: 0, left: 0, behavior: "smooth" };
+  }
 });
 
 export default router;
