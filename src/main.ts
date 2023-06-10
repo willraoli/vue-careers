5f48d3c3679b94955ca2595a2984f99c0b79bd27
William <48337511+willraoli@users.noreply.github.com>
2023-06-10 11:04:44 -0300

    Add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import { Icon } from "@iconify/vue";
+import { createPinia } from "pinia";
 import router from "@/router";
 import App from "@/App.vue";
 import "@/index.css";
 
-createApp(App).use(router).component("iconify-icon", Icon).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).component("iconify-icon", Icon).mount("#app");
