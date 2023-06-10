b7dd5967a28a8145df55ae5dd84314d8aefa67aa
William <48337511+willraoli@users.noreply.github.com>
2023-06-10 11:05:34 -0300

    Add jobsStore tests

---
+++ src/__tests__/unit/stores/jobs.test.ts
@@ -0,0 +1,34 @@
+import { createPinia, setActivePinia } from "pinia";
+import { useJobsStore } from "@/stores/jobs";
+import axios from "axios";
+import type { Mock } from "vitest";
+
+vi.mock("axios");
+
+describe("state", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it("armazena vagas", () => {
+    const store = useJobsStore();
+
+    expect(store.jobs).toEqual([]);
+  });
+});
+
+describe("actions", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe("FETCH_JOBS", () => {
+    it("faz a request na API e armazena as vagas recebidas", () => {
+      (axios.get as Mock).mockResolvedValue({ data: ["Vaga 1", "Vaga 2"] });
+      const store = useJobsStore();
+      store.FETCH_JOBS();
+
+      expect(store.jobs).toEqual(["Vaga 1", "Vaga 2"]);
+    });
+  });
+});
