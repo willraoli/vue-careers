f65e64d101694100aadb7c2c518024f0d623b1b5
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-18 19:30:25 -0300

    Add button role to CollapsibleAccordion

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -1,6 +1,12 @@
 <script lang="ts">
 export default {
   name: "CollapsibleAccordion",
+  props: {
+    header: {
+      type: String,
+      required: true
+    }
+  },
   data() {
     return {
       isOpen: false
@@ -21,8 +27,12 @@ export default {
 
 <template>
   <div class="border-b border-solid border-brand-gray-1 py-5">
-    <div class="flex flex-wrap items-center justify-between hover:cursor-pointer" @click="open">
-      <h3 class="select-none text-base font-semibold">Empresas</h3>
+    <div
+      class="flex flex-wrap items-center justify-between hover:cursor-pointer"
+      role="button"
+      @click="open"
+    >
+      <h3 class="select-none text-base font-semibold">{{ header }}</h3>
       <iconify-icon :icon="caretIcon" width="12" />
     </div>
     <div v-if="isOpen" class="mt-5 w-full">
