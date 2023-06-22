723497b5620d9e9897971eb34b6ac5febe68ee5e
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-22 15:53:02 -0300

    Add HeaderContainer component

---
+++ src/components/Shared/HeaderContainer.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+  name: "HeaderContainer"
+};
+</script>
+
+<template>
+  <div class="w-full border-b border-solid border-brand-gray-1">
+    <div class="mx-auto my-16 text-center">
+      <slot name="title">Title</slot>
+      <slot name="subtitle">Subtitle</slot>
+    </div>
+  </div>
+</template>
