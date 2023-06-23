61cd530b510167c76eaad86bd96a05432d062f45
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-23 14:09:07 -0300

    Add tests for uniqueOrgs and filteredJobsByOrg

--- src/__tests__/unit/stores/jobs.test.ts
+++ src/__tests__/unit/stores/jobs.test.ts
@@ -1,5 +1,6 @@
 import { createPinia, setActivePinia } from "pinia";
-import { useJobsStore } from "@/stores/jobs";
+import { UNIQUE_ORGS, useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
 import axios from "axios";
 import type { Mock } from "vitest";
 
@@ -29,6 +30,64 @@ describe("actions", () => {
       await store.FETCH_JOBS();
 
       expect(store.jobs).toEqual(["Vaga 1", "Vaga 2"]);
+    });
+  });
+});
+
+describe("getters", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe("UNIQUE_ORGS", () => {
+    it("retorna empresas únicas a partir de uma lista de vagas", () => {
+      const store = useJobsStore();
+      store.jobs = [
+        { organization: "Google" },
+        { organization: "Amazon" },
+        { organization: "Google" }
+      ];
+      const result = store[UNIQUE_ORGS];
+
+      expect(result).toEqual(new Set(["Google", "Amazon"]));
+    });
+  });
+
+  describe("filteredJobsByOrg", () => {
+    it("identifica vagas associadas com as empresas selecionadas", () => {
+      const jobsStore = useJobsStore();
+      const userStore = useUserStore();
+
+      jobsStore.jobs = [
+        { organization: "Google" },
+        { organization: "Amazon" },
+        { organization: "Microsoft" }
+      ];
+      userStore.selectedOrgs = ["Google", "Amazon"];
+
+      const result = jobsStore.filteredJobsByOrg;
+      expect(result).toEqual([{ organization: "Google" }, { organization: "Amazon" }]);
+    });
+
+    describe("quando o usuário não seleciona nenhuma empresa", () => {
+      it("retorna todas as empresas", () => {
+        const jobsStore = useJobsStore();
+        const userStore = useUserStore();
+
+        jobsStore.jobs = [
+          { organization: "Google" },
+          { organization: "Amazon" },
+          { organization: "Microsoft" }
+        ];
+        userStore.selectedOrgs = [];
+
+        const result = jobsStore.filteredJobsByOrg;
+        expect(result).toEqual([
+          { organization: "Google" },
+          { organization: "Amazon" },
+          { organization: "Microsoft" }
+        ]);
+      });
     });
   });
 });
