6b8a580002014d3d442cb2ece9807d41cc3b7053
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-23 14:10:41 -0300

    Add test for job counter on SubNav

--- src/__tests__/unit/components/Navigation/SubNav.test.ts
+++ src/__tests__/unit/components/Navigation/SubNav.test.ts
@@ -1,14 +1,19 @@
 import SubNav from "@/components/Navigation/SubNav.vue";
+import { useJobsStore } from "@/stores/jobs";
+import { createTestingPinia } from "@pinia/testing";
 import { render, screen } from "@testing-library/vue";
 
 describe("SubNav", () => {
   const renderSubNav = (routeName: string) => {
+    const pinia = createTestingPinia();
+    const jobsStore = useJobsStore();
     const $route = {
       name: routeName
     };
 
     render(SubNav, {
       global: {
+        plugins: [pinia],
         mocks: {
           $route
         },
@@ -17,20 +22,31 @@ describe("SubNav", () => {
         }
       }
     });
+
+    return { jobsStore };
   };
 
   describe("quando o usuário está na página de vagas", () => {
-    it("mostra a quantidade de vagas", () => {
-      renderSubNav("Vagas");
-      const jobCount = screen.getByText(/1234/i);
+    it("mostra a quantidade de vagas", async () => {
+      const { jobsStore } = renderSubNav("Vagas");
+      const jobNumber = 10;
+      // @ts-expect-error: Getter is read only
+      jobsStore.filteredJobsByOrg = Array(jobNumber).fill({});
+      const jobCount = await screen.findByText(jobNumber);
+
       expect(jobCount).toBeInTheDocument();
     });
   });
 
   describe("quando o usuário não está na página de vagas", () => {
     it("não exibe a quantidade de vagas", () => {
-      renderSubNav("Home");
-      const jobCount = screen.queryByText(/1234/i);
+      const { jobsStore } = renderSubNav("Home");
+      const jobNumber = 10;
+      // @ts-expect-error: Getter is read only
+      jobsStore.filteredJobsByOrg = Array(jobNumber).fill({});
+
+      const jobCount = screen.queryByText(jobNumber);
+
       expect(jobCount).not.toBeInTheDocument();
     });
   });
