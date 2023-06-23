71ac69e61d13bf1556b14a5187422652c8455d31
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-23 14:09:30 -0300

    Add `selectedOrgs` state to userStore

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,11 +2,15 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", {
   state: () => ({
-    isLoggedIn: false
+    isLoggedIn: false,
+    selectedOrgs: [] as string[]
   }),
   actions: {
     loginUser() {
       this.isLoggedIn = true;
+    },
+    addSelectedOrgs(orgs: string[]) {
+      this.selectedOrgs = orgs;
     }
   }
 });
