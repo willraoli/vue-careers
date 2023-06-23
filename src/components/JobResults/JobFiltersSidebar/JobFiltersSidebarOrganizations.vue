5f59e4c901832d148c796bc82db9d0b837291b03
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-23 14:13:05 -0300

    Add JobFiltersSidebarOrganizations component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -1,30 +1,47 @@
 <script lang="ts">
+import { mapState, mapActions } from "pinia";
+import { useJobsStore, UNIQUE_ORGS } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
 export default {
-  name: "JobFiltersSidebarOragnizations"
+  name: "JobFiltersSidebarOrganizations",
+  components: { CollapsibleAccordion },
+  data() {
+    return {
+      selectedOrgs: [] as string[]
+    };
+  },
+  computed: {
+    ...mapState(useJobsStore, [UNIQUE_ORGS])
+  },
+  methods: {
+    ...mapActions(useUserStore, ["addSelectedOrgs"]),
+    selectOrg() {
+      this.addSelectedOrgs(this.selectedOrgs);
+    }
+  }
 };
 </script>
 
 <template>
-  <div class="mt-5">
-    <fieldset>
-      <ul class="flex flex-grow flex-wrap">
-        <li class="h-8 w-1/2">
-          <input id="VueTube" type="checkbox" class="mr-3" />
-          <label for="VueTube" class="select-none">VueTube</label>
-        </li>
-        <li class="h-8 w-1/2">
-          <input id="Vue and Me" type="checkbox" class="mr-3" />
-          <label for="Vue and Me" class="select-none">Vue and Me</label>
-        </li>
-        <li class="h-8 w-1/2">
-          <input id="Vue Can Do It" type="checkbox" class="mr-3" />
-          <label for="Vue Can Do It" class="select-none">Vue Can Do It</label>
-        </li>
-        <li class="h-8 w-1/2">
-          <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-          <label for="Vue and a Half Men" class="select-none">Vue and a Half Men</label>
-        </li>
-      </ul>
-    </fieldset>
-  </div>
+  <collapsible-accordion header="Empresas">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-grow flex-wrap">
+          <li v-for="(org, idx) in UNIQUE_ORGS" :key="idx" class="h-8 w-1/2">
+            <input
+              :id="org"
+              v-model="selectedOrgs"
+              :value="org"
+              class="mr-3"
+              type="checkbox"
+              @change="selectOrg"
+            />
+            <label :for="org" class="select-none">{{ org }}</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
 </template>
