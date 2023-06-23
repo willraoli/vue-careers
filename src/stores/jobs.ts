57e04e706211dd9175db9706785efb15fbbb5064
willraoli <48337511+willraoli@users.noreply.github.com>
2023-06-23 14:08:49 -0300

    Add `uniqueOrgs` and `filteredJobsByOrg` getters to jobs store

--- src/stores/jobs.ts
+++ src/stores/jobs.ts
@@ -1,16 +1,41 @@
 import getJobs from "@/api/getJobs";
 import { defineStore } from "pinia";
+import { useUserStore } from "@/stores/user";
+
+type Job = {
+  organization: string;
+};
 
 export const FETCH_JOBS = "FETCH_JOBS";
+export const UNIQUE_ORGS = "UNIQUE_ORGS";
 
 export const useJobsStore = defineStore("jobs", {
   state: () => ({
-    jobs: []
+    jobs: [] as Job[]
   }),
   actions: {
     async [FETCH_JOBS]() {
       const jobs = await getJobs();
       this.jobs = jobs;
+    }
+  },
+  getters: {
+    [UNIQUE_ORGS](state) {
+      const uniqueOrgs = new Set<string>();
+      state.jobs.forEach((job: Job) => {
+        uniqueOrgs.add(job.organization);
+      });
+
+      return uniqueOrgs;
+    },
+    filteredJobsByOrg(state) {
+      const userStore = useUserStore();
+
+      if (userStore.selectedOrgs.length === 0) {
+        return state.jobs;
+      }
+
+      return state.jobs.filter((job) => userStore.selectedOrgs.includes(job.organization));
     }
   }
 });
