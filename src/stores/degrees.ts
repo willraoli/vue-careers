80f0417a920104d177a9a1e08d1cd291e3324de3
willraoli <48337511+willraoli@users.noreply.github.com>
2023-07-04 19:39:16 -0300

    Fix uniqueDegrees in `degrees` store

--- src/stores/degrees.ts
+++ src/stores/degrees.ts
@@ -5,7 +5,7 @@ import { computed, ref } from "vue";
 
 export const useDegreesStore = defineStore("degrees", () => {
   const degrees = ref<Degree[]>([]);
-  const uniqueDegrees = computed(() => degrees.value.map((item) => item.title));
+  const uniqueDegrees = computed(() => degrees.value.map((item) => item.degree));
 
   const fetchDegrees = async () => {
     const res = await getDegrees();
