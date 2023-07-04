754e4fb202af45ed1c1f0fb86d80c58d40947a1f
willraoli <48337511+willraoli@users.noreply.github.com>
2023-07-04 19:40:14 -0300

    Add `createDegree` helper

--- src/api/createDegree.ts
+++ src/api/createDegree.ts
@@ -2,6 +2,6 @@ import type { Degree } from "@/api/types";
 
 export const createDegree = (degree: Partial<Degree> = {}): Degree => ({
   id: 1,
-  title: "title",
+  degree: "title",
   ...degree
 });
