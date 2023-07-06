0417562a04c032c54d661f06110355ef6611b762
willraoli <48337511+willraoli@users.noreply.github.com>
2023-07-05 21:04:51 -0300

    Add `skillsSearch` getter and setter

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -6,15 +6,18 @@ export const useUserStore = defineStore("user", () => {
   const selectedOrgs = ref<string[]>([]);
   const selectedJobTypes = ref<string[]>([]);
   const selectedDegrees = ref<string[]>([]);
+  const skillsSearchTerm = ref("");
 
   const loginUser = () => (isLoggedIn.value = true);
   const addSelectedOrgs = (orgs: string[]) => (selectedOrgs.value = orgs);
   const addSelectedDegrees = (degrees: string[]) => (selectedDegrees.value = degrees);
   const addSelectedJobTypes = (jobTypes: string[]) => (selectedJobTypes.value = jobTypes);
+  const updateSkillsSearchTerm = (skill: string) => (skillsSearchTerm.value = skill);
   const clearFilters = () => {
     selectedDegrees.value = [];
     selectedJobTypes.value = [];
     selectedOrgs.value = [];
+    skillsSearchTerm.value = "";
   };
 
   return {
@@ -22,10 +25,12 @@ export const useUserStore = defineStore("user", () => {
     selectedOrgs,
     selectedJobTypes,
     selectedDegrees,
+    skillsSearchTerm,
     loginUser,
     addSelectedOrgs,
     addSelectedDegrees,
     addSelectedJobTypes,
+    updateSkillsSearchTerm,
     clearFilters
   };
 });
